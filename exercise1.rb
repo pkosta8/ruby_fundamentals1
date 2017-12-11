4fe0eb1d018ddf977ce45c1a714880ecea47db53
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-11 14:53:47 -0500

    updated2

--- exercise1.rb
+++ exercise1.rb
@@ -4,6 +4,25 @@
 
 #puts 2 puts 3 puts 2 + 3
 
-puts 2 + 3
-puts 2
-puts 3
+#puts 2 + 3
+#puts 2
+#puts 3
+
+
+#puts 'hello world'
+#puts "Hellow world"
+
+#puts "Ada Lovelace lived for #{1852 - 1815} years."
+# Ada Lovelace lived for 37 years.
+
+
+
+puts "Hello\t\tworld"
+
+
+puts "Hello\b\b\b\b\bGoodbye world"
+
+
+puts "Hello\rStart over world"
+
+puts "1. Hello\n2. World"
