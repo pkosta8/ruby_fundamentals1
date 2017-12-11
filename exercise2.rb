d8cb4b35455fb39bd989038c346c8ef2561ca0ea
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-11 15:40:00 -0500

    ex 2 updated4

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,12 @@
 
 #tip calculation
-puts  "Your tip amount is #{ 55 * 0.15 }  dollars"
+#puts  "Your tip amount is #{ 55 * 0.15 }  dollars"
+
+#puts  "Your amount with tip is #{ 55 * 0.15 + 55 }  dollars"
+
+
+puts "The total sum is  #{45628 * 7839}  ."
+
+# Ada Lovelace lived for 37 years.
+
+#(10 < 20 && 30 < 20) || !(10 == 11)
