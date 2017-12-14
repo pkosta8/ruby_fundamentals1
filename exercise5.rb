4b61ea89f5de214e4d37ffd03d6b24de278821f0
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-14 17:19:49 -0500

    ex5 final

--- exercise5.rb
+++ exercise5.rb
@@ -1,16 +1,21 @@
-travelled = 0
 
 # if decision == "exercise"
 
 puts "Would you like to walk or run?"
-decision = gets
 
-if decision == 'walk'
+
+decision = gets.chomp
+
+puts "how far have you travelled?"
+
+travelled = gets.chomp.to_i
+
+
+if decision == "walk"
 travelled = travelled + 1
 puts "You are #{travelled}Km from home."
 
-elsif decision == 'run'
+ elsif decision == 'run'
 travelled = travelled + 5
 puts "You are #{travelled}Km from home."
-end
-end
+ end
